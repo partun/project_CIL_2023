5b46437df22b514ea90d937ba817d03bc0b6d8db
Dominic <16167645+partun@users.noreply.github.com>
2023-07-08 11:08:16 +0200

    bert transfromer model

---
+++ bert/main.py
@@ -0,0 +1,197 @@
+from dataset import load_dataset, tokenize_dataset, load_and_tokenize_dataset
+import transformers
+import torch
+from tqdm import tqdm
+import numpy as np
+from torch.utils.data import DataLoader
+from sklearn import metrics
+import copy
+from torch import cuda
+import pickle
+
+np.random.seed(1)
+tokenizer_model = "distilbert-base-uncased"
+nn_model = "prajjwal1/bert-mini"
+device = "cuda" if cuda.is_available() else "cpu"
+TRAIN_BATCH_SIZE = 16
+VALID_BATCH_SIZE = 16
+EPOCHS = 1
+LEARNING_RATE = 1e-05
+
+
+class BERTClass(torch.nn.Module):
+    def __init__(self):
+        super(BERTClass, self).__init__()
+        self.l1 = transformers.BertModel.from_pretrained(
+            "prajjwal1/bert-mini", return_dict=False
+        )
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(256, 1)
+        self.l4 = torch.nn.Sigmoid()
+
+    def forward(self, ids, mask, token_type_ids):
+        _, output = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        output = self.l2(output)
+        output = self.l3(output)
+        output = self.l4(output)
+        return output
+
+
+def train_model(model, train_data, val_data):
+    loss_fn = torch.nn.BCELoss()  # binary cross entropy
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    # Hold the best model
+    best_acc = -np.inf  # init to negative infinity
+    best_weights = None
+
+    for epoch in range(EPOCHS):
+        model.train()
+        with tqdm(
+            train_data,
+            unit="batch",
+            mininterval=0,
+            miniters=200,
+        ) as bar:
+            bar.set_description(f"Epoch {epoch}")
+            for i, batch in enumerate(bar):
+                # take a batch
+
+                # forward pass
+                y_batch = batch["label"].to(device, dtype=torch.float32).reshape(-1, 1)
+                y_pred = model(
+                    batch["input_ids"].to(device, dtype=torch.long),
+                    batch["attention_mask"].to(device, dtype=torch.long),
+                    batch["token_type_ids"].to(device, dtype=torch.long),
+                )
+                loss = loss_fn(y_pred, y_batch)
+
+                # backward pass
+                optimizer.zero_grad()
+                loss.backward()
+                # update weights
+                optimizer.step()
+                # print progress
+
+                if i % 200 == 0:
+                    acc = (y_pred.round() == y_batch).float().mean()
+                    bar.set_postfix(loss=f"{float(loss):.3f}", acc=f"{float(acc):.3f}")
+
+        # evaluate accuracy at end of each epoch
+        model.eval()
+        with torch.no_grad():
+            correct_cnt = 0
+            cnt = 0
+            for val_batch in tqdm(val_data, desc="Validation"):
+                y_val = (
+                    val_batch["label"].to(device, dtype=torch.float32).reshape(-1, 1)
+                )
+                y_pred = model(
+                    val_batch["input_ids"].to(device, dtype=torch.long),
+                    val_batch["attention_mask"].to(device, dtype=torch.long),
+                    val_batch["token_type_ids"].to(device, dtype=torch.long),
+                )
+                cnt += len(y_val)
+                correct_cnt += int((y_pred.round() == y_val).int().sum())
+
+        acc = correct_cnt / cnt
+        print(f"Epoch {epoch} validation accuracy: {acc:.3f}")
+        if acc > best_acc:
+            best_acc = acc
+            best_weights = copy.deepcopy(model.state_dict())
+
+    # restore model and return best accuracy
+    model.load_state_dict(best_weights)
+    return best_acc
+
+
+def eval_model(model, train_data, val_data):
+    # evaluate accuracy at end of each epoch
+    model.eval()
+    with torch.no_grad():
+        correct_cnt = 0
+        cnt = 0
+        for batch in tqdm(val_data):
+            y = batch["label"].to(device, dtype=torch.float32).reshape(-1, 1)
+            y_pred = model(
+                batch["input_ids"].to(device, dtype=torch.long),
+                batch["attention_mask"].to(device, dtype=torch.long),
+                batch["token_type_ids"].to(device, dtype=torch.long),
+            )
+            cnt += len(y)
+            correct_cnt += int((y_pred.round() == y).int().sum())
+
+        acc = correct_cnt / cnt
+        print(f"validation accuracy: {acc:.3f}")
+
+        correct_cnt = 0
+        cnt = 0
+        for batch in tqdm(train_data):
+            y = batch["label"].to(device, dtype=torch.float32).reshape(-1, 1)
+            y_pred = model(
+                batch["input_ids"].to(device, dtype=torch.long),
+                batch["attention_mask"].to(device, dtype=torch.long),
+                batch["token_type_ids"].to(device, dtype=torch.long),
+            )
+            cnt += len(y)
+            correct_cnt += int((y_pred.round() == y).int().sum())
+
+        acc = correct_cnt / cnt
+        print(f"training accuracy: {acc:.3f}")
+
+
+def generate_predictions(model, test_data, output_file):
+    # evaluate accuracy at end of each epoch
+    model.eval()
+    with torch.no_grad():
+        ids = test_data["id"].to(device, dtype=torch.float32).reshape(-1, 1)
+        y_pred = model(
+            test_data["input_ids"].to(device, dtype=torch.long),
+            test_data["attention_mask"].to(device, dtype=torch.long),
+            test_data["token_type_ids"].to(device, dtype=torch.long),
+        )
+
+        print(y_pred.head())
+        print(ids.head())
+
+
+def save_model(model, path):
+    torch.save(model.state_dict(), path)
+
+
+def load_model(model, path):
+    model.load_state_dict(torch.load(path))
+
+
+def main():
+    print(f"running bert model using {device}")
+
+    model = BERTClass()
+    model.to(device)
+
+    dataset = load_and_tokenize_dataset(
+        tokenizer_model, device, frac=1, use_full_dataset=True, train_size=0.8
+    )
+
+    training_loader = DataLoader(
+        dataset["train"],
+        batch_size=TRAIN_BATCH_SIZE,
+        shuffle=True,
+        num_workers=0,
+    )
+    validation_loader = DataLoader(
+        dataset["validation"],
+        batch_size=VALID_BATCH_SIZE,
+        shuffle=False,
+        num_workers=0,
+    )
+
+    load_model(model, "bert_mini_2_epoch_full.pkl")
+    train_model(model, training_loader, validation_loader)
+    save_model(model, "bert_mini_3_epoch_full.pkl")
+
+    eval_model(model, training_loader, validation_loader)
+
+
+if __name__ == "__main__":
+    main()
